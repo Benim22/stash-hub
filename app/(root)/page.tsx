5ed85aac355e2dcd82181369fa2f8673c8fe1e85
Benim22 <113353252+Benim22@users.noreply.github.com>
2025-01-21 18:37:08 +0100

    first

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-amber-100">
+      <h1 className="text-3xl font-bold text-brand">
+        StashHub - All your files in one secure hub.{" "}
+      </h1>
+    </div>
+  );
+}
